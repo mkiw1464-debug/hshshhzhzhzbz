268ec38a3e842ce79d6d69fc732d674d3572e58e
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-23 21:20:17 +0800

    Remove redundant include of FFEXCheat.h

--- Sources/FFEXModMenu.m
+++ Sources/FFEXModMenu.m
@@ -7,7 +7,7 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #include "FFEXCore.h"
-#include "FFEXCheat.h"
+#include "FFEXCore.h"
 
 // ══════════════════════════════════════════════════════════════
 // THEME
